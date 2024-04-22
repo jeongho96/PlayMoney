54f871edb2f30400819238badbc97100a3538a17
jeongho <jeongholee0918@gmail.com>
2024-04-22 19:02:52 +0900

    init project

---
+++ src/main/java/com/reboot/playmoney/PlayMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.reboot.playmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayMoneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlayMoneyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reboot/playmoney/PlayMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reboot.playmoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayMoneyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
